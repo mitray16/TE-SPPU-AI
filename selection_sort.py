dc5c47f51792b24f898e481d29be1ab673a957e7
mitray16 <99380404+mitray16@users.noreply.github.com>
2023-05-29 22:41:25 +0530

    Update selection_sort.py

--- selection_sort.py
+++ selection_sort.py
@@ -1,14 +1,18 @@
 def selection_sort(arr):
-  n = len(arr)
-  for i in range(n-1):
-    min_idx=i
-    for j in range (1+1,n):
-      if arr[j] < arr[min_idx}
-                      min_idx = j
-   if min_idx != i:
-     arr[i],arr[min_idx] = arr[min_idx],arr[i]
-  return arr
-                      
-array = input("enter array (space sepearted) : ").split()
-array = [int(num) for num in array)
-sorted_array = selection_sort(array)
+    n = len(arr)
+
+    for i in range(n - 1):
+        min_index = i
+
+        for j in range(i + 1, n):
+            if arr[j] < arr[min_index]:
+                min_index = j
+
+        arr[i], arr[min_index] = arr[min_index], arr[i]
+
+    return arr
+numbers = input("enter array (space seperated) : ").split()
+numbers = [int(num) for num in numbers]
+# numbers = [5, 2, 9, 1, 7]
+sorted_numbers = selection_sort(numbers)
+print("Sorted numbers:", sorted_numbers)
